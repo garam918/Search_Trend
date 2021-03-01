9e9e40083a4f8316805c8b709be252252a18d4b2
garam918 <62510764+garam918@users.noreply.github.com>
2021-03-01 16:24:30 +0900

    Initial commit

---
+++ app/src/main/java/com/example/garam/search_trend/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.garam.search_trend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
