6da397bde38aa893e1c040efec6810968140b15d
garam918 <62510764+garam918@users.noreply.github.com>
2021-03-07 01:33:24 +0900

    Feat : 원하는 그룹명, 키워드, 시작 & 종료 날짜를 정해서 검색

--- app/src/main/java/com/example/garam/search_trend/MainActivity.kt
+++ app/src/main/java/com/example/garam/search_trend/MainActivity.kt
@@ -1,99 +1,90 @@
 package com.example.garam.search_trend
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import com.example.garam.search_trend.data.KeywordInfoData
-import com.example.garam.search_trend.data.ResponseData
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.garam.search_trend.databinding.ActivityMainBinding
-import com.example.garam.search_trend.network.NetworkController
-import com.example.garam.search_trend.network.NetworkService
 import com.example.garam.search_trend.viewModel.SearchViewModel
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
-import com.google.gson.JsonObject
-import com.google.gson.JsonParser
-import org.json.JSONArray
-import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
-    private val networkService : NetworkService by lazy {
-        NetworkController.instance.networkService
-    }
-
-    private lateinit var entry : ArrayList<Double>
-
-//    private lateinit var viewModel : SearchViewModel
+    private lateinit var viewModel : SearchViewModel
 
     private lateinit var binding: ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.lifecycleOwner = this
 
-        val json = JSONObject()
+        viewModel = ViewModelProvider(this).get(SearchViewModel::class.java)
+        binding.viewModel = viewModel
 
-        val jsonArray = JSONArray()
+        binding.startDate.setOnClickListener {
 
-        jsonArray.put("한글")
-        jsonArray.put("korean")
+            val datePicker = DatePickerDialog(this)
+            datePicker.show().apply {
+                datePicker.setOnDateSetListener { datePicker, year, monthOfYear, dayOfMonth ->
 
-        json.put("groupName","한글")
-        json.put("keywords",jsonArray)
-
-        val testObj = JsonParser().parse(json.toString()) as JsonObject
-
-        val test2 = KeywordInfoData("2017-04-01","2017-04-30","date", arrayListOf(testObj),null,null,null)
-
-        Log.e("왜 안돼",test2.toString())
-
-
-        var entries : ArrayList<Entry> = ArrayList()
-        entries.add(Entry(0F,0F))
-        val dataSet = LineDataSet(entries,"testLabel")
-
-        var data : LineData = LineData(dataSet)
-
-
-        binding.searchButton.setOnClickListener {
-
-            networkService.trendSearch("ClientId","ClientSecret",test2).enqueue(object : Callback<ResponseData>{
-                override fun onFailure(call: Call<ResponseData>, t: Throwable) {
-                    Log.e("Fda",t.message.toString())
-                }
-
-                override fun onResponse(call: Call<ResponseData>, response: Response<ResponseData>) {
-                    Log.e("왜지",response.code().toString())
-                    Log.e("zzz",response.body().toString())
-                    entry = ArrayList()
-
-                    for (i in 0 until response.body()!!.results[0].data.size) {
-                        Log.e("ㅇㄻ",response.body()!!.results[0].data.size.toString())
-                        entry.add(response.body()!!.results[0].data[i].ratio.toDouble())
-
-                        data.addEntry(Entry(i.toFloat(),response.body()!!.results[0].data[i].ratio.toFloat()),0)
-                        data.notifyDataChanged()
-                        binding.lineChart.notifyDataSetChanged()
-                        binding.lineChart.invalidate()
-
-                        binding.lineChart.data = LineData(dataSet)
-
-                        Log.e("무야호",response.body()!!.results[0].data[i].ratio)
+                    val year = year.toString()
+                    val month = when(monthOfYear+1) {
+                        in 1 .. 9 -> {
+                            "0${monthOfYear+1}"
+                        }
+                        else -> {
+                            "${monthOfYear+1}"
+                        }
                     }
+                    val day = when(dayOfMonth) {
+                        in 0 .. 9 -> {
+                            "0$dayOfMonth"
+                        }
+                        else -> {
+                            "$dayOfMonth"
+                        }
+                    }
+                    val startDateInfo = "$year-$month-$day"
+                    viewModel.setStartDate(startDateInfo)
 
                 }
-            })
+            }
 
+        }
 
+        binding.endDate.setOnClickListener {
+
+            val datePicker = DatePickerDialog(this)
+            datePicker.show().apply {
+                datePicker.setOnDateSetListener { datePicker, year, monthOfYear, dayOfMonth ->
+
+                    val year = year.toString()
+                    val month = when(monthOfYear+1) {
+                        in 1 .. 9 -> {
+                            "0${monthOfYear+1}"
+                        }
+                        else -> {
+                            "${monthOfYear+1}"
+                        }
+                    }
+                    val day = when(dayOfMonth) {
+                        in 0 .. 9 -> {
+                            "0$dayOfMonth"
+                        }
+                        else -> {
+                            "$dayOfMonth"
+                        }
+                    }
+                    val startDateInfo = "$year-$month-$day"
+                    viewModel.setEndDate(startDateInfo)
+
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/garam/search_trend/viewModel/SearchViewModel.kt
+++ app/src/main/java/com/example/garam/search_trend/viewModel/SearchViewModel.kt
@@ -1,6 +1,105 @@
 package com.example.garam.search_trend.viewModel
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.garam.search_trend.data.KeywordInfoData
+import com.example.garam.search_trend.data.ResponseData
+import com.example.garam.search_trend.network.NetworkController
+import com.example.garam.search_trend.network.NetworkService
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import org.json.JSONArray
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class SearchViewModel : ViewModel() {
+class SearchViewModel(application: Application) : AndroidViewModel(application) {
+
+
+    private val networkService : NetworkService by lazy {
+        NetworkController.instance.networkService
+    }
+
+    val searchInfo = MutableLiveData<KeywordInfoData>()
+
+    private lateinit var entry : ArrayList<Double>
+
+    val groupName = MutableLiveData<String>()
+    val keywords = MutableLiveData<String>()
+ //   val dateRange = MutableLiveData<String>()
+
+    val startDate = MutableLiveData<String>()
+    val endDate = MutableLiveData<String>()
+
+    fun setStartDate(date : String) {
+        startDate.value = date
+    }
+
+    fun setEndDate(date: String) {
+        endDate.value = date
+    }
+
+    fun searchTrend(lineChart: LineChart) {
+
+        val json = JSONObject()
+        val jsonArray = JSONArray()
+
+        for(element in (keywords.value!!.split(","))) {
+            jsonArray.put(element)
+        }
+
+        json.put("groupName",groupName.value.toString())
+        json.put("keywords",jsonArray)
+
+        val testObj = JsonParser().parse(json.toString()) as JsonObject
+
+        val test2 = KeywordInfoData(startDate.value.toString(),endDate.value.toString()
+            ,"date", arrayListOf(testObj),null,null,null)
+
+        Log.e("왜 안돼2",test2.toString())
+
+
+        var entries : ArrayList<Entry> = ArrayList()
+        entries.add(Entry(0F,0F))
+        val dataSet = LineDataSet(entries,groupName.value.toString())
+
+        var data : LineData = LineData(dataSet)
+
+        networkService.trendSearch("ClientId","ClientSecret",test2).enqueue(object :
+            Callback<ResponseData> {
+            override fun onFailure(call: Call<ResponseData>, t: Throwable) {
+                Log.e("Fda",t.message.toString())
+            }
+
+            override fun onResponse(call: Call<ResponseData>, response: Response<ResponseData>) {
+                Log.e("왜지",response.code().toString())
+                Log.e("zzz",response.body().toString())
+                entry = ArrayList()
+
+                for (i in 0 until response.body()!!.results[0].data.size) {
+
+                    entry.add(response.body()!!.results[0].data[i].ratio.toDouble())
+
+                    data.addEntry(Entry(i.toFloat(),response.body()!!.results[0].data[i].ratio.toFloat()),0)
+                    data.notifyDataChanged()
+                    lineChart.notifyDataSetChanged()
+                    lineChart.invalidate()
+
+                    lineChart.data = LineData(dataSet)
+
+                    Log.e("무야호",response.body()!!.results[0].data[i].ratio)
+                }
+
+            }
+        })
+    }
+
+
 }
